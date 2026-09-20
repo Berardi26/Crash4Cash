8048a9059b2521e945c54474d23a5836506add81
Berardi26 <berardi26@gmail.com>
2026-09-20 08:05:00 +0000

    Fix instant in-game cashout button event emission

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
+  const handleCashout = () => {
+    socketRef.current?.emit('cashout');
+    setCashedOut(true);
+  };
 import React, { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
